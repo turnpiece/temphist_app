45fc76caa21efa2b9cba4d83f5d4e367f6d8d69a
Paul Jenkins <paul@turnpiece.com>
2025-08-14 17:10:04 -0400

    Improved loading states

--- test/main_test.dart
+++ test/main_test.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:temphist_app/main.dart';
 import 'dart:async';
-import 'package:flutter/foundation.dart';
 
 void main() {
   testWidgets('Finds any text', (WidgetTester tester) async {
