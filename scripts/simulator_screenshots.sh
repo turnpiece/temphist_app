9c6011537bd28c3d27dba10543d5136236a2795a
Paul Jenkins <paul@turnpiece.com>
2025-09-04 15:55:43 +0100

    Set Simulator for App Store screenshots

---
+++ scripts/simulator_screenshots.sh
@@ -0,0 +1,8 @@
+xcrun simctl status_bar booted override \
+  --time 9:41 \
+  --dataNetwork wifi \
+  --wifiBars 3 \
+  --cellularMode active \
+  --cellularBars 4 \
+  --batteryState charged \
+  --batteryLevel 100
