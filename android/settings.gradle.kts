0e282e4d2957ab65f66bd60b8da5ebff0f8b36fd
Paul Jenkins <paul@turnpiece.com>
2026-06-22 19:54:21 +0100

    Add app promo images

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -23,6 +23,7 @@ plugins {
     id("com.google.gms.google-services") version("4.3.15") apply false
     // END: FlutterFire Configuration
     id("org.jetbrains.kotlin.android") version "2.2.20" apply false
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
 }
 
 include(":app")
